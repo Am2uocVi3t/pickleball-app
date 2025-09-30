39ad43505c2f5305053fb3d658bf35ba1df9b394
Am2uocVi3t <tyviet1609@gmail.com>
2025-09-30 22:41:18 +0700

    Moved streamlit logo position

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ from utils.stats import show_stats_page
 from utils.funds import show_fund_page
 
 
-st.set_page_config(page_title="Pickleball App", page_icon="https://img.freepik.com/premium-vector/pickleball-paddles-logo-pickleball-club-icon-vector_854827-129.jpg?w=2000", layout="centered")
-
+st.set_page_config(
+    page_title="Pickleball App", 
+    page_icon="https://img.freepik.com/premium-vector/pickleball-paddles-logo-pickleball-club-icon-vector_854827-129.jpg?w=2000", 
+    layout="centered"
+    )
+st.markdown(
+    """
+    <div style="position: absolute; top: 1rem; right: 1rem; z-index: 100;">
+        <img src="https://img.freepik.com/premium-vector/pickleball-paddles-logo-pickleball-club-icon-vector_854827-129.jpg?w=2000" width="60" style="border-radius: 50%;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 menu = st.sidebar.radio(
     "Menu", 
     ["Nhập thông tin", "Bảng chi tiết", "Thống kê", "Quỹ nhóm", "Hội viên"]
